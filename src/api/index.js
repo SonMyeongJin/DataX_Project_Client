dd133d1c113d96d7968d313c6fff6ef4aa96d834
SonMyeongJin <smj2802@naver.com>
2025-03-10 15:30:09 +0900

    [Fix] Cors error

--- src/api/index.js
+++ src/api/index.js
@@ -1,13 +1,13 @@
 import axios from "axios";
 
 const apiClient = axios.create({
-    baseURL: "http://43.203.118.99:3000", // Rails 서버 주소
+    baseURL: "http://43.203.118.99:3000",
     headers: {
         "Content-Type": "application/json",
     },
+    withCredentials: false  // 인증 정보 전송 비활성화
 });
 
-// 요청을 보내기 전에 JWT 토큰이 있으면 헤더에 추가
 apiClient.interceptors.request.use((config) => {
     const token = localStorage.getItem("token");
     if (token) {
